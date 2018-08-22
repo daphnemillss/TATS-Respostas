b319804676d185db22f3e51f5a81bea5df15bf25
daphnemillss <dafnymastrelly@gmail.com>
2018-08-22 15:00:01 -0300

    Segundo Commit TDD

---
+++ atividade04/PrimeiroCommitTDD/Atividade04TDD/src/atividade04/CalculadoraDeSalario.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade04;
+
+/**
+ *
+ * @author daphnemillss
+ */
+public class CalculadoraDeSalario {
+
+    public double calculaSalario(Funcionario f) {
+        switch (f.getCargo()) {
+            case "DESENVOLVEDOR":
+                if (f.getSalarioBase() >= 3000) {
+                    return f.getSalarioBase() - (f.getSalarioBase() * 0.2);
+                } 
+                else {
+                    return f.getSalarioBase() - (f.getSalarioBase() * 0.1);
+                }
+            
+        }
+        return 0;
+
+        }
+}
